a26c0ce0fe97b325a010ea243258bec2121b4cdd
tyrotoxism1 <janzenm@oregonstate.edu>
2024-06-23 13:30:44 -0700

    blink led project setup

---
+++ projects/blink_led/src/main.c
@@ -0,0 +1,14 @@
+#include "stm32f4xx.h"
+
+int main(void) {
+    // Initialization code
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;  // Enable GPIOA clock
+    GPIOA->MODER |= GPIO_MODER_MODE5_0;   // Set PA5 as output
+
+    while (1) {
+        // Toggle LD2 LED
+        GPIOA->ODR ^= GPIO_ODR_OD5;
+        // Delay
+        for (volatile int i = 0; i < 1000000; i++);
+    }
+}
