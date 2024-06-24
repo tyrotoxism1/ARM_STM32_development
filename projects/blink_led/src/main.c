1af27552a068ba7f7b1594df3f44d591d9ec8e10
tyrotoxism1 <janzenm@oregonstate.edu>
2024-06-23 22:47:00 -0700

    Used PA6 to toggle external LED

--- projects/blink_led/src/main.c
+++ projects/blink_led/src/main.c
@@ -3,11 +3,11 @@
 int main(void) {
     // Initialization code
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;  // Enable GPIOA clock
-    GPIOA->MODER |= GPIO_MODER_MODE5_0;   // Set PA5 as output
+    GPIOA->MODER |= GPIO_MODER_MODE6_0;   // Set PA5 as output
 
     while (1) {
         // Toggle LD2 LED
-        GPIOA->ODR ^= GPIO_ODR_OD5;
+        GPIOA->ODR ^= GPIO_ODR_OD6;
         // Delay
         for (volatile int i = 0; i < 1000000; i++);
     }
